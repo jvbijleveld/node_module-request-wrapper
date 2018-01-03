3223c5d9cafebdfccf54fc2a6bd27bf409a321d7
jeffrey <jeffrey@vanbijleveld.nl>
2018-01-03 09:21:55 -0500

    new datatypes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var moment = require('moment');
 
 exports.wrap = function(entity, request){
 	var ret = {};
@@ -5,12 +6,35 @@ exports.wrap = function(entity, request){
 	for(var key in entity){
 		console.log("parsing field " + JSON.stringify(key));
 		if(request.body[key] !== undefined){
-			if(entity[key].type == "integer"){
-				ret[key] = parseInt(request.body[key]);
-			}else{
-				ret[key] = request.body[key];
+			switch(entity[key].type){
+				case "integer":
+					ret[key] = parseInt(request.body[key]);
+					break;
+					
+				case "float":
+					ret[key] = parseFloat(request.body[key]);
+					break;
+					
+				case "date":
+					ret[key] = formatDate(request.body[key], entity[key].format);
+					break;
+					
+				case "datetime":
+					ret[key] = formatDateTime(request.body[key], entity[key].format);
+					break;
+					
+				default :
+					ret[key] = request.body[key];
 			}
 		}
 	}
 	return ret;
+}
+
+formatDate = function(inp, format){
+	return moment(inp, format);
+}
+
+formatDateTime = function(inp, format){
+	return moment(inp.replace('T',' '), format);
 }
